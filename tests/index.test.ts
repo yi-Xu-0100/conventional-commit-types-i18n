a3a4744b856879bc2e63a67015c75427979a3356
yi_Xu <xjy123456120@163.com>
2021-02-22 15:40:26 +0800

    feat: ⚡️ Exported function returns json instead of string
    ref: https://github.com/vivaxy/vscode-conventional-commits/issues/47

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,17 +3,15 @@ import { resolve } from 'path';
 import { conventionalCommitsTypes } from '../src/index';
 
 describe('conventionalCommitsTypes() function test', () => {
-  it('should return content of en.json without input', () => {
-    const defaultResult = conventionalCommitsTypes();
-    const getEnJSON = readFileSync(resolve(__dirname, '../locale/en.json'), 'utf8');
-    expect(defaultResult).toEqual(getEnJSON);
-  });
   it.each([
+    ['en.json', undefined],
     ['en.json', 'en'],
     ['zh-cn.json', 'zh-cn']
   ])("should return content of %s when input is '%s'", (fileName, locale) => {
     const result = conventionalCommitsTypes(locale);
-    const getLocaleJSON = readFileSync(resolve(__dirname, `../locale/${fileName}`), 'utf8');
-    expect(result).toEqual(getLocaleJSON);
+    const getLocaleJSON = JSON.parse(
+      readFileSync(resolve(__dirname, `../locale/${fileName}`), 'utf8')
+    );
+    expect(result.types).toEqual(getLocaleJSON.types);
   });
 });
