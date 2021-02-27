ad632662fe304c1c948e2f1212d0c3f91c7fa6f2
yi_Xu <xjy123456120@163.com>
2021-02-27 14:05:05 +0800

    test: ✅ update test for more loaclization

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,36 +3,33 @@ import { resolve } from 'path';
 import { conventionalCommitsTypes } from '../src/index';
 import getTypes from '../src/index';
 
-describe('conventionalCommitsTypes function test', () => {
+describe('conventionalCommitsTypes and getTypes function test', () => {
   it.each([
     ['en.json', undefined],
     ['en.json', ''],
     ['en.json', 'unknown-locale'],
     ['en.json', 'en'],
+    ['zh-cn.json', 'zh-cn'],
+    ['zh-tw.json', 'zh-tw'],
+    ['fr.json', 'fr'],
+    ['de.json', 'de'],
     ['it.json', 'it'],
-    ['zh-cn.json', 'zh-cn']
+    ['es.json', 'es'],
+    ['ja.json', 'ja'],
+    ['ko.json', 'ko'],
+    ['ru.json', 'ru'],
+    ['bg.json', 'bg'],
+    ['hu.json', 'hu'],
+    ['pt-br.json', 'pt-br'],
+    ['tr.json', 'tr'],
+    ['es.json', 'es']
   ])("should return content of %s when input is '%s'", (fileName, locale) => {
     const result = conventionalCommitsTypes(locale);
+    const _result = getTypes(locale);
     const getLocaleJSON = JSON.parse(
       readFileSync(resolve(__dirname, `../locale/${fileName}`), 'utf8')
     );
     expect(result.types).toEqual(getLocaleJSON.types);
-  });
-});
-
-describe('getTypes function test', () => {
-  it.each([
-    ['en.json', undefined],
-    ['en.json', ''],
-    ['en.json', 'unknown-locale'],
-    ['en.json', 'en'],
-    ['it.json', 'it'],
-    ['zh-cn.json', 'zh-cn']
-  ])("should return content of %s when input is '%s'", (fileName, locale) => {
-    const result = getTypes(locale);
-    const getLocaleJSON = JSON.parse(
-      readFileSync(resolve(__dirname, `../locale/${fileName}`), 'utf8')
-    );
-    expect(result.types).toEqual(getLocaleJSON.types);
+    expect(_result.types).toEqual(getLocaleJSON.types);
   });
 });
